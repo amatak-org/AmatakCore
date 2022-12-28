997f87e13b958bfa59d5ac401dd38f90e5183c10
ronyman.com <ronyman.com@outlook.com>
2022-12-28 23:15:03 +1100

    Updates

--- amatak-cli/bin/cli.js
+++
@@ -1,8 +0,0 @@
-import {Command} from '@oclif/core'
-export class amatak extends Command {
-  public static enableJsonFlag = true
-  public async run(): Promise<{ message: string }> {
-    console.log('amatak')
-    return { message: 'Amatak Serve' }
-  }
-}
--- amatak-cli/src/index.ts
+++
@@ -1,7 +0,0 @@
-export {run} from '@oclif/core'
-import {Hook} from '@oclif/core'
-
-const hook: Hook<'init'> = async function (options) {
-  console.log(`amatak start ${options.id}`)
-}
-export default hook
